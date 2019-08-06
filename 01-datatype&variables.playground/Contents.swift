d231a60ff9ce18b47f2267bfef4ab217bda60bf1
Anna-Paula <anna-paula@Apples-MacBook-Pro-108.local>
2019-08-06 10:12:23 -0400

    Initial commit

---
+++ 01-datatype&variables.playground/Contents.swift
@@ -0,0 +1,85 @@
+import UIKit
+
+//Examples of strings
+"puppies"
+"1234"
+"Hello world"
+
+//Example of a variable
+var address = "10 Hudson Yard"
+address = "48th floor"
+var companyName : String
+companyName = "Best Company Ever"
+
+//Example of a constant
+let planet = "Earth"
+
+//Examples of joining variables and string
+var string1 = "Welcome"
+var string2 = "AnnaPaula"
+
+var welcome1 = "\(string1) \(string2)"
+var welcome2 = string1 + " " + string2
+
+print (welcome1)
+print ("Here is my sentence")
+
+//Practice
+var first = "Karlie"
+var last = "Kloss"
+
+print ("\(first)\(last)")
+print ("\(last)\(first)")
+print (first + " " + last)
+print (last + " " + first + " " + last + " " + first)
+print ("I love \(first)!")
+
+//Examples of integers
+1
+54
+-16
+
+//Examples of floats & dubles
+0.13
+3.04
+2.1
+
+var integer = 4.0
+var double = 5.0
+
+4 * 4
+4 * integer
+4 * double
+integer * double
+
+//Practice with numbers
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+var average = (a + b + c + d )/4
+((a + b) * c )/d
+(a + b * c / d)
+
+//Practice Data Types
+//name - constant
+//adress - variable
+//birthday - constant
+//grade level - variable
+//age - variable
+//eye color - constant
+// favorite color - variable
+
+var earns = 10.25
+var hour = 20.0
+
+(earns * hour)
+
+var name = "Justin Bieber"
+print ("Happy Birthday dear \(name)")
+
+
+
+
+
---
+++ MyPlayground.Conditionals.playground/Contents.swift
@@ -0,0 +1,65 @@
+//Comparison statement
+5 < 3
+12 > 7
+6 != 7
+//7 == "7"
+
+"karlie" == "karlie"
+"karlie" == "karlie kloss"
+"Karlie" == "karlie"
+
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+//Example of conditions
+var dogAge = 5
+if dogAge < 2 {
+    print ("you are a puppy!🐶")
+}
+else if dogAge > 12 {
+    print ("You are olderly")
+}
+else {
+    print("You are awesome!")
+}
+
+//Control Flow
+var favoriteFood = "Chipotle"
+if favoriteFood == "Chipotle"{
+    print ("You are happy")
+}
+else if favoriteFood != "Starbucks"{
+    print ("You are not happy")
+}
+else if favoriteFood == "Pizza"{
+    print ("Happy")
+}
+
+//Practice Conditionals
+var hasAccount = "True"
+if hasAccount == "True"{
+print ("Please log in")
+}
+
+//Part2
+var studentGrade = 11
+if studentGrade <= 5 {
+    print ("Elementary")
+}
+else if studentGrade <= 9 {
+    print ("Middle")
+}
+else if studentGrade <= 12 {
+    print ("High school")
+}
+
+
+
+
+
+
+
+
+
+
